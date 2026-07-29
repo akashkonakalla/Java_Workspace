20f56eee2241b9b950076b100a0451508c715018
Akash <konakalla.akash@gmail.com>
2026-07-29 09:01:36 +0530

    add : SQL practice

---
+++ SQL/practice1.sql
@@ -0,0 +1,9 @@
+/*
+Mysql
+1.Display employee name, job, and salary sorted by job ascending and salary descending.
+2.Display all employees sorted by hire date (oldest to newest).
+*/
+
+select *from emp order by ename ,sal desc;
+select *from emp order by hiredate ;
+
