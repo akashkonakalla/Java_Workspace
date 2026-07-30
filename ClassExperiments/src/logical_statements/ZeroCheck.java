16402fac68244d1c168c0c65b0daf02ede180918
Akash <konakalla.akash@gmail.com>
2026-07-30 22:28:58 +0530

    add : java logical statements

---
+++ ClassExperiments/src/logical_statements/ZeroCheck.java
@@ -0,0 +1,15 @@
+package logical_statements;
+
+import java.util.Scanner;
+
+public class ZeroCheck {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number");
+		int a = sc.nextInt();
+		if (a == 0)
+			System.out.println("NegativeNumber");
+	}
+
+}
