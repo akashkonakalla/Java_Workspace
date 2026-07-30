e7348bbb5d676c291e45ea8813a5087e55163775
Akash <konakalla.akash@gmail.com>
2026-07-30 22:36:14 +0530

    add : java logical statements

---
+++ ClassExperiments/src/logical_statements/SmallestOfThree.java
@@ -0,0 +1,26 @@
+package logical_statements;
+
+import java.util.Scanner;
+
+public class SmallestOfThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number");
+		int a = sc.nextInt();
+		System.out.println("enter b number");
+		int b = sc.nextInt();
+		System.out.println("enter c number");
+		int c = sc.nextInt();
+		if (a == b && a == c)
+			System.out.println("equal");
+		else if (a > b && c > b)
+			System.out.println("b is small");
+		else if (a < b && a < c)
+			System.out.println("a is small");
+		else
+			System.out.println("c is small");
+	}
+
+}
