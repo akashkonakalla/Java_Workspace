348ca5ce7d1cac46c1339898690fc1f067d768f4
Akash <konakalla.akash@gmail.com>
2026-07-30 22:27:27 +0530

    add : java logical statements

---
+++ ClassExperiments/src/logical_statements/NegativeNumber.java
@@ -0,0 +1,14 @@
+package logical_statements;
+
+import java.util.Scanner;
+
+public class NegativeNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number");
+		int a = sc.nextInt();
+		if (a<0)
+			System.out.println("NegativeNumber");
+	}
+	}
+	
