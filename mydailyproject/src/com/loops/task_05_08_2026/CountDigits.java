4795a2470fe67d2ea79fa7f3beebba7590c71690
Akash <konakalla.akash@gmail.com>
2026-08-05 10:50:36 +0530

    Update : Count and Sum of digits in a given number on 05/08/2026

--- mydailyproject/src/com/loops/task_05_08_2026/CountDigits.java
+++ mydailyproject/src/com/loops/task_05_08_2026/CountDigits.java
@@ -1,5 +1,20 @@
 package com.loops.task_05_08_2026;
 
+/*
+ * Today Task
+Java 
+1.Count the digits in a number.
+Input
+Enter a number: 12345
+Output
+Number of Digits: 5
+2.Find the sum of digits of a number.
+
+Input
+Enter a number: 1234
+Output
+Sum of Digits: 10
+ */
 import java.util.Scanner;
 
 public class CountDigits {
@@ -8,11 +23,24 @@ public class CountDigits {
 		if(n==0)
 			return 1;
 		int c=0;
+		
 		while(n>0) {
 			n=n/10;
 			c++;
+			
 		}
 		return c;
+	}
+	int sumofDigits(int n){
+		int sum=0;
+		while(n>0) {
+			int rem=n%10;
+			sum+=rem;
+			n/=10;
+		}
+		
+		return sum;
+		
 	}
 
 	public  void main(String[] args) {
@@ -21,6 +49,7 @@ public class CountDigits {
 		System.out.println("enter the number to find digits");
 		int a=sc.nextInt();
 		System.out.println("The number of digits in a given number is : "+ countDigits(a));
+		System.out.println("The number of sum of digits in a given number is : "+ sumofDigits(a));
 		
 
 	}
