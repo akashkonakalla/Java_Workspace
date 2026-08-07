21c4301060ef24c463f5d6013b8fff9c19a33151
Akash <konakalla.akash@gmail.com>
2026-08-07 11:46:12 +0530

    add : Loops experiment on 07/08/2026

---
+++ ClassExperiments/src/loops/Loop1.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class Loop1 {
+ public static void main(String[] args) {
+	 for (byte i = 1; i <= 12; i++) {
+		    System.out.println(i);
+		}
+	 int num = 599077679;
+	 short b = (short) num;
+
+	 System.out.println(b);
+}
+}
