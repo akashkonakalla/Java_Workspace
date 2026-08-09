be4d5361a1719052f863416461c0ec8536294320
Akash <konakalla.akash@gmail.com>
2026-08-09 22:33:35 +0530

    update : array reverse using the 2 pointers

--- ClassExperiments/src/com/dsa/ArraySwap.java
+++ ClassExperiments/src/com/dsa/ArraySwap.java
@@ -1,10 +1,22 @@
 package com.dsa;
 
+import java.util.Arrays;
+
 public class ArraySwap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arr= {1,3,5,7,9,10};
+		int left=0;
+		int right=arr.length-1;
+		System.out.println("Array before reverse is "+Arrays.toString(arr));
+		while(left<right) {
+			int temp=arr[left];
+			arr[left]=arr[right];
+			arr[right]=temp;
+			left++;
+			right--;
+		}
+		System.out.println("Array after reverse is "+Arrays.toString(arr));
 	}
-
 }
