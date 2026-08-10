0153f50c3db627bd2a477431996e9fe198b4e12f
Akash <konakalla.akash@gmail.com>
2026-08-10 10:33:50 +0530

    Update : Even numbers between 0 to 100 using the if condition on 08/08/2026

--- mydailyproject/src/com/loops/task_10_08_2026/Even.java
+++ mydailyproject/src/com/loops/task_10_08_2026/Even.java
@@ -13,9 +13,22 @@ Mysql:
  */
 public class Even {
 	public static void main(String[] args) {
+		
+		// using for with increment +2
+		System.out.println("Using the increment +2");
 		for(int i=2;i<=100;i+=2) {
 			System.out.println(i);
 		}
+		//using if condition
+		System.out.println("\nUsing the if condition");
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
+		
 	}
 
 }
