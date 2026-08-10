14f0011aff251d251b9b7351e10752bb2f9bff9e
Akash <konakalla.akash@gmail.com>
2026-08-10 10:30:48 +0530

    add : Even numbers between 0 to 100 on 10/08/2026

---
+++ mydailyproject/src/com/loops/task_10_08_2026/Even.java
@@ -0,0 +1,21 @@
+package com.loops.task_10_08_2026;
+/*
+ * Today's Assignment
+Java
+1.write a java program to print even numbers between 1 to 100.
+2.write a java program to Find the sum of numbers from 1 to 100.
+Output: 5050
+
+Mysql:
+1.Display employees who are working in the DALLAS location.
+
+2.Display employee names along with their department names.
+ */
+public class Even {
+	public static void main(String[] args) {
+		for(int i=2;i<=100;i+=2) {
+			System.out.println(i);
+		}
+	}
+
+}
