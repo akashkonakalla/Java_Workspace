74abd4060b1af65587b4a2245411207abb2a29fd
Akash <konakalla.akash@gmail.com>
2026-08-11 10:39:12 +0530

    add : Given number is prime or not on 11/08/2026

---
+++ mydailyproject/src/com/loops/task_11_08_2026/PrimeOrNot.java
@@ -0,0 +1,40 @@
+package com.loops.task_11_08_2026;
+
+import java.util.Scanner;
+
+/*
+ * Today's Assignment
+Java
+1.write a java program to print given number is prime or not.
+
+2.write a java program to print prime numbers between range 1to 50.
+ */
+public class PrimeOrNot {
+	static boolean isPrime(int n){
+		boolean prime=true;
+		if(n==0 || n ==1)
+			return false;
+		for(int i=2;i<=n/2;i++) {
+			if(n%2==0)
+			{
+				prime=false;
+				break;
+			}
+		}
+		return prime;
+	}
+	
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number to check Prime or not");
+		int n=sc.nextInt();
+ 
+		if(isPrime(n)==false)
+		System.out.println(n+ " is a not an prime number");
+		else
+			System.out.println(n+ " is a prime number");
+		
+		
+	}
+}
