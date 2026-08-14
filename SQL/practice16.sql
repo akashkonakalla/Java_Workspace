7f562ab99916b66422cd8ae45669c2a3bc4a29d3
Akash <konakalla.akash@gmail.com>
2026-08-14 12:50:34 +0530

    Update : SQL Practice on joins and union

--- SQL/practice16.sql
+++ SQL/practice16.sql
@@ -5,4 +5,4 @@ SQL -
  use dailytasks;
  (select *from emp as e left outer join dept as d on e.deptno =d.deptno)
  union 
- (select *from emp d right outer join dept e on e.deptno=d.deptno) ;
+ (select *from emp as e right outer join dept d on e.deptno=d.deptno) ;
