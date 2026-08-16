6f79d954212061ea3bdb9bdb59c8d733e4cc6a8a
Akash <konakalla.akash@gmail.com>
2026-08-16 23:58:21 +0530

    add : fist occurrence of the target in java

---
+++ ClassExperiments/src/com/dsa/FirstOccurence.java
@@ -0,0 +1,38 @@
+package com.dsa;
+
+public class FirstOccurence {
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 2, 2, 4, 6, 8, 8, 10 };
+		int target = 2;
+
+		System.out.println("Array:");
+		for (int x : arr) {
+			System.out.print(x + " ");
+		}
+
+		System.out.println("\n\nTarget = " + target);
+
+		System.out.println("First occurrence       : " + firstOccurrence(arr, target));
+	}
+
+	static int firstOccurrence(int[] arr, int target) {
+		int low = 0, high = arr.length - 1;
+		int ans = -1;
+
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+
+			if (arr[mid] == target) {
+				ans = mid;
+				high = mid - 1; // search left
+			} else if (arr[mid] < target) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+
+		return ans;
+	}
+}
