5ebca1e1c4916b847f787f69c3fdd0c13dd494d7
Akash <konakalla.akash@gmail.com>
2026-08-17 16:57:27 +0530

    add : Sum of 2 different size arrays

---
+++ ClassExperiments/src/com/arrays/AddArrays.java
@@ -0,0 +1,25 @@
+package com.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddArrays {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int[] a1= {1,2,3};
+	int[] a2= {4,5,6,7,8};
+	int max=Math.max(a1.length, a2.length);
+	int[] a3= new int[max];
+	for(int i=0;i<max;i++) {
+		if(i>a1.length) {
+			a3[i]=a2[i];
+		}
+		else if(i>a2.length) {
+			a3[i]=a1[i];
+		}
+		else
+			a3[i]=a2[i]+a1[i];
+	}
+	System.out.println("The Sum of Arrays is : "+ Arrays.toString(a3));
+}
+}
