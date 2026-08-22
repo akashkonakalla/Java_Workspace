a2be18e6c58f8b5546bdf57abd67dc1b6c06c37e
Akash <konakalla.akash@gmail.com>
2026-08-22 23:02:50 +0530

    add : SQL Case study 4 on 22/08/2026

---
+++ SQL/casestudy4.sql
@@ -0,0 +1,12 @@
+/*
+1. Find Departments located in cities having more than one department.
+2. Employees who joined earliest in each department.
+*/
+select *from dept;
+
+SELECT d.deptno,d.dname
+FROM dept d
+where loc in (
+select d1.loc from dept d1 group by d1.loc having count(d1.deptno)>1 );
+
+select *from emp as e where e.hiredate = (select min(e1.hiredate) from emp as e1 where e1.deptno=e.deptno);
