d035b0a56ebbb70611aa29f20fefb46179aa8edf
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-23 23:48:48 +0530

    update : Jagged Array on 23/08/2026

--- ClassExperiments/src/com/arrays/JaggedArray.java
+++ ClassExperiments/src/com/arrays/JaggedArray.java
@@ -3,17 +3,22 @@ package com.arrays;
 import java.util.Scanner;
 
 public class JaggedArray {
-public static void main(String[] args) {
-	System.out.println("Enter the number of rows");
-	Scanner sc=new Scanner(System.in);
-	int r=sc.nextInt();
-	int[][] ar= new int[r][r];
-	for(int[] i: ar) {
-		for(int n: i) {
-			System.out.print(n + " ");
+	public static void main(String[] args) {
+		System.out.println("Enter the number of rows");
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int[][] ar = new int[3][];
+
+		ar[0] = new int[4];
+		ar[1] = new int[3];
+		ar[2] = new int[5];
+
+		for (int[] i : ar) {
+			for (int n : i) {
+				System.out.print(n + " ");
+			}
+			System.out.println();
 		}
-		System.out.println();
+
 	}
-	
-}
 }
