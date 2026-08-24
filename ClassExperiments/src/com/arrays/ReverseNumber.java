90775161067570598a4d2c146c534c2139b1bfc0
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-24 17:30:14 +0530

    Update : Reverse Number in an array using 2 pointers.

--- ClassExperiments/src/com/arrays/ReverseNumber.java
+++ ClassExperiments/src/com/arrays/ReverseNumber.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	int n=sc.nextInt();
 	int[] arr= new int[n];
+	System.out.println("Enter the elements in the array");
 	for(int i=0;i<n;i++) {
 		arr[i]=sc.nextInt();
 	}
