0174e773064c3fce1cffe2fc1f2a6d7314985b68
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-24 17:35:38 +0530

    add : Add values in a given matrix on 24/08/2026

---
+++ mydailyproject/src/com/arrays/task_24_08_2026/AddMatrix.java
@@ -0,0 +1,30 @@
+package com.arrays.task_24_08_2026;
+
+import java.util.Scanner;
+
+/*
+ * *Today's Assignment*
+Java-
+1.Write a Java program to read a 3 × 3 matrix and find the sum of all its elements.
+Input: 1 2 3
+           4 5 6
+           7 8 9
+Output : Sum of all elements = 45
+2.Write a Java program to read the elements of a 3 × 3 matrix using the Scanner class and display the matrix in the same format.
+
+ */
+public class AddMatrix {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the rows of the matrix");
+		int r=sc.nextInt();
+		System.out.println("Enter the columns of the matrix");
+		int c=sc.nextInt();
+		for(int i=0;i<r;i++) {
+			for(int j=0;j<c;j++) {
+				System.out.println("Enter the values of matrix");
+			}
+		}
+		
+	}
+}
