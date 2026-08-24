ff1b7d7f19b5542d77720d6b0bb6f7f3a691bd90
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-24 17:40:29 +0530

    Update : Add numbers in a given Matrix on 24/08/2026

--- mydailyproject/src/com/arrays/task_24_08_2026/AddMatrix.java
+++ mydailyproject/src/com/arrays/task_24_08_2026/AddMatrix.java
@@ -15,16 +15,21 @@ Output : Sum of all elements = 45
  */
 public class AddMatrix {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the rows of the matrix");
-		int r=sc.nextInt();
+		int r = sc.nextInt();
 		System.out.println("Enter the columns of the matrix");
-		int c=sc.nextInt();
-		for(int i=0;i<r;i++) {
-			for(int j=0;j<c;j++) {
-				System.out.println("Enter the values of matrix");
+		int c = sc.nextInt();
+		int sum=0;
+		int[][] matrix = new int[r][c];
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				System.out.printf("Enter the values of matrix matrix[%d][%d] :",i,j);
+				matrix[i][j]=sc.nextInt();
+				sum+=matrix[i][j];
 			}
 		}
-		
+		System.out.println("The sum of the numbers in the matrix : "+sum);
+
 	}
 }
