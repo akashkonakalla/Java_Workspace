bccdab9b7689e6e48bbf5d41628140eaf9b89a99
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-24 20:11:26 +0530

    add: Right diagonal sum program

---
+++ mydailyproject/src/com/arrays/task_24_08_2026/DiagonalRightSum.java
@@ -0,0 +1,29 @@
+package com.arrays.task_24_08_2026;
+
+//Sum of the Diagonal numbers
+
+
+import java.util.Scanner;
+
+
+public class DiagonalRightSum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the rows of the matrix");
+		int r = sc.nextInt();
+		System.out.println("Enter the columns of the matrix");
+		int c = sc.nextInt();
+		int sum = 0;
+		int[][] matrix = new int[r][c];
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				System.out.printf("Enter the values of matrix matrix[%d][%d] :", i, j);
+				matrix[i][j] = sc.nextInt();
+				if((i + j == c - 1))
+				sum += matrix[i][j];
+			}
+		}
+		System.out.println("The sum of the Diagonal numbers in the matrix : " + sum);
+
+	}
+}
