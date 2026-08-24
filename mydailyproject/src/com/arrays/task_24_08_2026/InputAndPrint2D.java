fd7371202dfe37e05ccc53a841756bb144c18ecd
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-24 17:52:29 +0530

    add : Take the input and print the matrix on 26/08/2026

---
+++ mydailyproject/src/com/arrays/task_24_08_2026/InputAndPrint2D.java
@@ -0,0 +1,37 @@
+package com.arrays.task_24_08_2026;
+
+import java.util.Scanner;
+
+/*
+ * *Today's Assignment*
+Java-
+1.Write a Java program to read a 3 × 3 matrix and find the sum of all its elements.
+Input: 1 2 3
+           4 5 6
+           7 8 9
+Output : Sum of all elements = 45
+2.Write a Java program to read the elements of a 3 × 3 matrix using the Scanner class and display the matrix in the same format.
+
+ */
+public class InputAndPrint2D {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the rows of the matrix");
+		int r = sc.nextInt();
+		System.out.println("Enter the columns of the matrix");
+		int c = sc.nextInt();
+		int[][] matrix = new int[r][c];
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				System.out.printf("Enter the values of matrix matrix[%d][%d] :",i,j);
+				matrix[i][j]=sc.nextInt();
+			}
+		}
+        for(int i[] : matrix) {
+        	for(int n: i) {
+        		System.out.print(n + "\t");
+        	}
+        	System.out.println();
+        }
+	}
+}
