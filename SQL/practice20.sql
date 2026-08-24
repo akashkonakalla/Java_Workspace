7a5d7eb711ecb62ff06bb8379b25c8b983ce839b
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-24 17:22:29 +0530

    Update : SQL Practice

--- SQL/practice20.sql
+++ SQL/practice20.sql
@@ -4,8 +4,15 @@ SQL-
 2.Display departments having more than one manager.
  */
  
- use dailytasks;
+use dailytasks;
+select *from emp order by sal desc;
+select distinct sal from emp order by sal desc;
+
+SELECT * FROM emp e WHERE (
+    SELECT COUNT(distinct sal)
+    FROM emp e2
+    WHERE e2.sal > e.sal
+) =4  -1;
+
  
- select *from emp order by sal desc;
- select *from emp e where 1=(select count(sal) from emp e2 where e2.sal>e.sal );
-select  distinct deptno from emp e2 where mgr is not null  group by deptno,mgr having count(*)>1;
+select  deptno,count(distinct mgr)as 'manager count ',(select dname from dept where dept.deptno=e2.deptno) as dname from emp e2 where mgr is not null  group by deptno having count(distinct mgr) > 1;
