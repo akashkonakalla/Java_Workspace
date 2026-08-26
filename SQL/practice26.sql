a4a0a09b045665f76fb580de976ca48ecf5559ec
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-26 17:42:39 +0530

    add : SQL Practice on 26/08/2026

---
+++ SQL/practice26.sql
@@ -0,0 +1,16 @@
+/*
+*MySQL*-
+1. Find the Emoloyees who have 5th highest salary in the emp table
+2. ⁠find the employees who are having last 25% salary groups in  the table
+*/
+
+use dailytasks;
+
+with sal_count as (
+select *,dense_rank() over(order by sal desc) as rnk from emp 
+)
+select empno, ename, job,mgr, hiredate,sal, comm,deptno from sal_count where rnk=5 ;
+
+with result as (
+select *,ntile(4) over(order by sal desc) as lastppl from emp
+) select *from result where lastppl =4;
