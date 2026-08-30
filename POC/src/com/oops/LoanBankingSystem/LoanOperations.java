c31c55b636deaf8ab990a5868e41febe3ea4abf6
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-30 18:14:34 +0530

    add : LoanOperation Interface for LoanBankingSystem on 30/08/2026

---
+++ POC/src/com/oops/LoanBankingSystem/LoanOperations.java
@@ -0,0 +1,17 @@
+package com.oops.LoanBankingSystem;
+
+public interface LoanOperations {
+
+	public abstract double getSalary();
+
+	public double getCalculateROI(int cibil);
+
+	public int getAge();
+
+	public abstract boolean isPhoneValid();
+
+	public abstract boolean isAadhaarValid();
+
+	public abstract boolean isPanValid();
+
+}
