2e419a7b78f23a38f0ebe0200cf93844010c94c6
Akash Konakalla <konakalla.akash@gmail.com>
2026-08-31 12:46:49 +0530

    add : Person Operations Interface

---
+++ POC/src/com/oops/HospitalManagementSystem/PersonOperations.java
@@ -0,0 +1,13 @@
+package com.oops.HospitalManagementSystem;
+
+public interface PersonOperations {
+
+	public abstract int getAge();
+
+	public abstract boolean isPhoneValid();
+
+	public abstract boolean isIdValid();
+
+	public abstract void displayDetails();
+
+}
