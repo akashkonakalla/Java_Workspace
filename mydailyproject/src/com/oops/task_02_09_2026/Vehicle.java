f044edb63d3230fe079099720e46363072b2f638
Akash Konakalla <konakalla.akash@gmail.com>
2026-09-02 17:21:13 +0530

    feat : add Vehicle parent class on 02/09/2026

---
+++ mydailyproject/src/com/oops/task_02_09_2026/Vehicle.java
@@ -0,0 +1,22 @@
+package com.oops.task_02_09_2026;
+
+/*
+ * *Today's Assignment*
+Java-
+1.Create a Vehicle class with a start() method. Create a Car class that inherits Vehicle and has a drive() method.
+2.Create a Person class with name and age. Create a Student class that inherits from Person and adds collegeName.
+
+Sql-
+1. Find employees whose salary is greater than the average salary of their department, but less than the maximum salary of their department, and whose manager earns less than the employee's salary.
+ */
+public class Vehicle {
+
+	void start() {
+		System.out.println("Vehicle Started");
+	}
+
+	void drive() {
+		System.out.println("Vehicle is driving");
+	}
+
+}
