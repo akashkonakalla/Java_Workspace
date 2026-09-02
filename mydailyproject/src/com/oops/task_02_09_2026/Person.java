4d1393025901f8a8287fbfc08f789278ab31bb40
Akash Konakalla <konakalla.akash@gmail.com>
2026-09-02 18:03:10 +0530

    feat : add Person class with common fields on 02/09/2026

---
+++ mydailyproject/src/com/oops/task_02_09_2026/Person.java
@@ -0,0 +1,35 @@
+package com.oops.task_02_09_2026;
+/*
+ * *Today's Assignment*
+Java-
+1.Create a Vehicle class with a start() method. Create a Car class that inherits Vehicle and has a drive() method.
+2.Create a Person class with name and age. Create a Student class that inherits from Person and adds collegeName.
+
+Sql-
+1. Find employees whose salary is greater than the average salary of their department, but less than the maximum salary of their department, and whose manager earns less than the employee's salary.
+ */
+
+public class Person {
+	private int age;
+	private String name;
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void collegeName() {
+		System.out.println("default college");
+	}
+}
