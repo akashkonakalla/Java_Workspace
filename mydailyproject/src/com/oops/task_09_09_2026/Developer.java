9ea54899125f25a436ddefdfc6c7131589d81c70
Akash Konakalla <konakalla.akash@gmail.com>
2026-09-10 14:35:02 +0530

    feat : add Developer, Tester and Manager classes implements Attendance

---
+++ mydailyproject/src/com/oops/task_09_09_2026/Developer.java
@@ -0,0 +1,10 @@
+package com.oops.task_09_09_2026;
+
+public class Developer implements Attendance {
+
+	@Override
+	public void markAttendance() {
+		System.out.println("Developer Attendance");
+	}
+
+}
---
+++ mydailyproject/src/com/oops/task_09_09_2026/Tester.java
@@ -0,0 +1,10 @@
+package com.oops.task_09_09_2026;
+
+public class Tester implements Attendance {
+
+	@Override
+	public void markAttendance() {
+		System.out.println("Tester Attendance");
+	}
+
+}
