a90d08d9818a74f4462c5496c420f3cce12b11c5
Akash Konakalla <konakalla.akash@gmail.com>
2026-09-11 23:45:38 +0530

    feat : add CurrentAccount class

---
+++ mydailyproject/src/com/oops/task_11_09_2026/CurrentAccount.java
@@ -0,0 +1,12 @@
+package com.oops.task_11_09_2026;
+
+public class CurrentAccount extends Account{
+
+	@Override
+	void calculateInterest() {
+		// TODO Auto-generated method stub
+		System.out.println("Current Account Interest : 3%");
+	}
+
+	
+}
