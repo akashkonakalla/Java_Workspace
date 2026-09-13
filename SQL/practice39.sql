be7a2d034a37b1609ffa21c0fdb9748fd918a03c
Akash Konakalla <konakalla.akash@gmail.com>
2026-09-13 23:47:27 +0530

    feat : add SQL Practice on 13/09/2026

--- SQL/practice39.sql
+++ SQL/practice39.sql
@@ -3,6 +3,13 @@ select ceil(1001.1);
 select floor(1000.9);
 select power(2,4);
 select sqrt(4);
-select rand(10);
+select rand();
 select log(4);
+select power(2,10);
+select exp(2);
+select abs(-4);
+select abs(99.99);
+select log10(10);
+select log2(2);
+select sign(-100);
 
